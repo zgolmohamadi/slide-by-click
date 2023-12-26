e323e38b8426f427772702c3bafa18efa5382513
zgolmohamadi <zgh402@gmail.com>
2023-12-26 17:22:20 +0330

    add: course data sample

--- src/services/index.js
+++ src/services/index.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 
-
 export const retrieveCourses = async () => {
-    const response = await axios.get("https://app.tarazorg.ir/api/qb/v4/test_unit");
-    return response.data;
-  };
+  const response = await axios.get(
+    "https://app.tarazorg.ir/api/qb/v4/test_unit"
+  );
+  return response.data;
+};
 
+export const retrieveCoursesItems = async (courseType) => {
+  // const response = await axios.get("https://app.taraz.org/api/qb/v4/unit/c205ce98-84f4-427e-9555-ccd95ae651d9/unit_tree/no_children/?parent_isnull=true");
+  // return response.data;
 
-
-
-
-  export const retrieveCoursesItems = async (courseType) => {
-    // const response = await axios.get("https://app.taraz.org/api/qb/v4/unit/c205ce98-84f4-427e-9555-ccd95ae651d9/unit_tree/no_children/?parent_isnull=true");
-    // return response.data;
-
-    //mock data
-    return import('../data/index.js').then((module) => {
-      return module.getSampleData(courseType);
-    });
-  };
+  //mock data
+  return import("../data/index.js").then((module) => {
+    return module.getSampleData(courseType);
+  });
+};
